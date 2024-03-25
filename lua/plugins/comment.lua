8985118f191bcf1a50ca72fca904eea415deeeea
VIISAUS <8653971+ouyang-jing@user.noreply.gitee.com>
2024-03-25 18:57:21 +0800

    flash

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,1 +1,19 @@
 require('Comment').setup()
+
+require('flash').setup{
+  labels = "ASDFGHJKLQWERTYUIOPZXCVBNM",
+  search = {
+    mode = "fuzzy",
+    max_length = false,
+  },
+  label = {
+    prefix = "🔥",
+    after = false,
+    before = true,
+    reuse = "all",
+    rainbow = {
+      enable = true,
+      shade = 9,
+    }
+  }
+}
